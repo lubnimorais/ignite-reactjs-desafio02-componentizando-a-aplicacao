cee8b0d2d102289143fc54a5a1c872d1db993641
Lubni Morais <lubni.morais@gmail.com>
2021-12-12 15:39:47 -0300

    chore: Completed challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
+import { MovieCard } from './MovieCard'
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IContentProps {
+  selectedGenre: {
+    title: string;
+  }
+  movies: Array<MovieProps>;
+}
+
+export function Content({ selectedGenre, movies}: IContentProps) {
   // Complete aqui
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
